d42b990dcdc4cdf6725efe4c61f3e2a93aa41f37
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-07 16:15:09 -0600

    identified variable types

--- indentation_test.py
+++ indentation_test.py
@@ -67,3 +67,13 @@ MAX_LIMIT = 1000
 # Python Data Types and type() Function
 
 #Task 1: Code Correction
+
+variable_a = "Hello, World!" # string
+variable_b = 23 # integer
+variable_c = 3.14 # float
+variable_d = True # boolean
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
