0ba87c099cd81d4c8e5e4c0f43ef53ecdec3dce8
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-07 16:10:44 -0600

    finished comment related tasks

--- indentation_test.py
+++ indentation_test.py
@@ -55,4 +55,15 @@ Python your victory is close at hand
 you're the cream of the crop
 """
 
-# 
+# Python Naming Convention Practice
+
+# Task 1: Code Correction
+
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+# Python Data Types and type() Function
+
+#Task 1: Code Correction
