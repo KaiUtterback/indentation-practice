13f9e009883a94a1df798d8f0529c35696171665
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-07 15:50:10 -0600

    Poem

--- indentation_test.py
+++ indentation_test.py
@@ -24,3 +24,17 @@ if mood == "excited":
     print("Yay! Let's have fun.")
 else:
     print("Let's find something fun to do!")
+
+# Python Comments and Multi Line Practice
+# Objective: Practice using single-line and multi-line comments
+    
+# Task 1: Create a poem
+    
+# Python, in the real of code you shine, 
+# With simplicity an dgrace, you're truly devine
+# Your sipmle code is like no other
+# We were meant to work with one another
+# If ever Python I should leave
+# Just come along and follow me
+
+ 
