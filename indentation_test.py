c555c7d88e3067caecf0e905e2d30688e1c80c70
KaiUtterback <156825843+KaiUtterback@users.noreply.github.com>
2024-02-07 15:21:47 -0600

    Update indentation_test.py

--- indentation_test.py
+++ indentation_test.py
@@ -1,9 +1,11 @@
 # Python indentation test: Practice with if statements
 
+# Task 1: Code Correction
 weather = "sunny"
 
 if weather == "sunny":
     print("Wear sunglasses!")
 else:
     print("Take an umbrella!")
-    
+
+# Task 2: Your Mood Today
